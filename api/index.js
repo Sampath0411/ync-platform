e95924826669653c7e99542645f2e6b221f967e9
Sampath Satya Saran <sampathbokka2008@gmail.com>
2026-07-22 22:15:56 +0530

    Debug: capture actual error in Vercel handler

--- api/index.js
+++ api/index.js
@@ -1,23 +1,18 @@
 process.env.SERVERLESS = '1';
 process.env.VERCEL = '1';
 
-const serverless = require('serverless-http');
-
-let handler = null;
-
 module.exports = async function vercelHandler(req, res) {
   try {
-    if (!handler) {
-      const app = require('../server/index');
-      handler = serverless(app);
-    }
+    const serverless = require('serverless-http');
+    const app = require('../server/index');
+    const handler = serverless(app);
     return await handler(req, res);
   } catch (err) {
     console.error('CRITICAL:', err.message);
     console.error(err.stack);
     if (!res.headersSent) {
       res.writeHead(500, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ success: false, message: 'Server error' }));
+      res.end(JSON.stringify({ success: false, message: err.message }));
     }
   }
 };
